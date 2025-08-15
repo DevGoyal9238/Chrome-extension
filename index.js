04d0669f2e121e587194eb6f2c33955d5caf9841
DevGoyal9238 <devgoyal9238@gmail.com>
2025-08-15 14:58:15 +0530

    Created method to add items in list

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ let myLeads= []
 const inputEl=document.getElementById("input-el")
 const inputBtn=document.getElementById("input-btn")
 const ulEl=document.getElementById("ul-el")
+
 inputBtn.addEventListener("click", function(){
     let invalue= inputEl.value
     myLeads.push(invalue)
     console.log(myLeads)
+
     for(let i=0;i<myLeads.length;i++){
-    ulEl.textContent+=myLeads[i]+" "
+    // ulEl.innerHTML += "<li>"+myLeads[i]+"</li"
+    const li=document.createElement("li")
+    li.textContent+=myLeads[i]
+    ulEl.append(li)
 }
 })
 
