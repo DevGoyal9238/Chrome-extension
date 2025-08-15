ddc0d77fc0e30de978ed7b56d8ba2677e6cc6706
DevGoyal9238 <devgoyal9238@gmail.com>
2025-08-15 09:43:34 +0530

    basic JS

---
+++ index.js
@@ -0,0 +1,14 @@
+let myLeads= []
+const inputEl=document.getElementById("input-el")
+const inputBtn=document.getElementById("input-btn")
+const ulEl=document.getElementById("ul-el")
+inputBtn.addEventListener("click", function(){
+    let invalue= inputEl.value
+    myLeads.push(invalue)
+    console.log(myLeads)
+    for(let i=0;i<myLeads.length;i++){
+    ulEl.textContent+=myLeads[i]+" "
+}
+})
+
+
