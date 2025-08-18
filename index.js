2446422eeb1410964690074b2238818c9670fe9d
DevGoyal9238 <devgoyal9238@gmail.com>
2025-08-18 21:55:02 +0530

    Cleared the input bar after each entry

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ const ulEl=document.getElementById("ul-el")
 inputBtn.addEventListener("click", function(){
     let invalue= inputEl.value
     myLeads.push(invalue)
-    console.log(myLeads)
+    inputEl.value=""
+    renderLeads()
 
-    for(let i=0;i<myLeads.length;i++){
-    // ulEl.innerHTML += "<li>"+myLeads[i]+"</li"
-    const li=document.createElement("li")
-    li.textContent+=myLeads[i]
-    ulEl.append(li)
-}
 })
+
+function renderLeads(){
+let listItems = ""
+    for(let i=0;i<myLeads.length;i++){
+    listItems+= "<li>" + myLeads[i] + "</li>"
+}
+
+ulEl.innerHTML = listItems
+
+}
 
 
